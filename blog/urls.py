6e589e1c2eb9fac63171d085fce1001542b7785c
karpiotr90 <92075179+karpiotr90@users.noreply.github.com>
2023-08-20 20:55:59 +0200

    add RSS channel

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     #path('', views.PostListView.as_view(), name='post_list'),
@@ -13,4 +14,5 @@ urlpatterns = [
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
     path('tag/<slug:tag_slug>/',
          views.post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
