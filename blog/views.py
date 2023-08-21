317afceae8a6103a502116d40426f9d700a3616d
karpiotr90 <92075179+karpiotr90@users.noreply.github.com>
2023-08-21 20:56:37 +0200

    implement trigram similarity search engine

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
@@ -105,8 +105,8 @@ def post_search(request):
         if form.is_valid():
             query = form.cleaned_data['query']
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
 
     return render(request,
                   'blog/post/search.html',
